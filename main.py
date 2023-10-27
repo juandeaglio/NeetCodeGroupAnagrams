b5541fa8f0618893a5e52203ee1e2ae757a10e07
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-10-27 16:03:48 -0400

    Using a dict matches this algorithm problem exactly

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
+from abc import abstractmethod, ABC
 from collections import defaultdict
-from typing import List
+from typing import List, Protocol
 
 
-class Solution:
+class GroupingStrategy(Protocol):
+    @abstractmethod
+    def solve(self, strs: List[str]) -> List[List[str]]:
+        pass
+
+
+class NaiveGroupingStrategy(GroupingStrategy, ABC):
     def __init__(self):
         self.memoized_anagrams = {}
 
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    def solve(self, strs):
         groups = []
-        words_added = {}
+        words_added = set()
         while strs:
             word = strs[0]
-            strs.remove(word)
+            strs = strs[1:]
             if word not in words_added:
-                words = self.get_all_anagrams(word, strs)
-                for word_to_add in words:
-                    if len(strs) > 0:
-                        strs.remove(word_to_add)
-
-                    words_added[word_to_add] = True
-
-                words_added[word] = True
-                words.append(word)
-                groups.append(words)
-
+                anagrams = self.get_all_anagrams(word, strs)
+                words_added.update(anagrams)
+                words_added.add(word)
+                anagrams.append(word)
+                groups.append(anagrams)
         return groups
-
 
     def get_all_anagrams(self, word, strs):
         anagrams = []
+
         for other_word in strs:
-            if self.is_anagram(word, other_word):
+            if other_word not in self.memoized_anagrams and self.is_anagram(word, other_word):
                 anagrams.append(other_word)
 
         return anagrams
@@ -38,8 +39,8 @@ class Solution:
     def is_anagram(self, word, other_word):
         if len(word) != len(other_word):
             return False
-        
-        if other_word in self.memoized_anagrams:
+
+        if (word, other_word) in self.memoized_anagrams or (other_word, word) in self.memoized_anagrams:
             return True
 
         total_counts = defaultdict(int)
@@ -51,7 +52,26 @@ class Solution:
             total_counts[char] -= 1
 
         result = all(value == 0 for value in total_counts.values())
+
         if result:
-            self.memoized_anagrams[other_word] = result
+            # Memoize the anagram pair.
+            self.memoized_anagrams[(word, other_word)] = True
 
         return result
+
+
+class SortedGroupingStrategy(GroupingStrategy, ABC):
+    def solve(self, strs: List[str]) -> List[List[str]]:
+        anagram_groups = defaultdict(list)
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            anagram_groups[sorted_word].append(word)
+
+        return list(anagram_groups.values())
+
+class Solution:
+    def __init__(self, strategy=SortedGroupingStrategy):
+        self.strategy = strategy()
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        return self.strategy.solve(strs)
